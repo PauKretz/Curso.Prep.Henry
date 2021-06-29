45729547b7e628015b7783238fdedb7a58203c81
PauKretz <paulakretz@gmail.com>
2021-06-29 11:04:39 -0300

    concepts04-js-iii

---
+++ 04-JS-III/conceptsArrays.js
@@ -0,0 +1,7 @@
+Arrays son una lista de elementos, los elementos pueden ser todos los tipos que ya vimos. Ej, for, while, if. 
+Se definen usan corchetes []
+Podemos acceder a sus elementos a traves de un indice que comienza en 0. 
+Las variables que ya conocemos nos sirve pra guardar un arreglo. 
+Tiene las funciones Push: que recibe o empuja el un argumento y lo colocar en la ultima posicion libre. Tiene la funcion Pop: que saca el ultimo elemento y te lo devuelve.Arrays.Arrays
+Los arreglos son como cajitas, le digo, entra a tal cajita y fijate que hay. 
+Tambien tenemos shift y unshift, que hacen lo mismo que push y pop pero operan con el primer elemento. 
