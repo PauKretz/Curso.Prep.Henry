f4b625c669cedd7584590711b23affc9d534d9a8
unknown <you@example.com>
2021-06-30 11:35:44 -0300

    concepts05-js-iv

---
+++ 05-JS-IV/concepts05-js-iv.js
@@ -0,0 +1,8 @@
+const { EMPTY_COMMANDS } = require("simple-git/src/lib/tasks/task")
+const { agregarMetodoCalculoDescuento } = require("./homework/homework")
+
+Objetos: Javascript esta diseñado sobre un paradigma simple basado en objetos. Un objeto es una coleccion de propiedades, es un contenedor de datos. Los objetos se realizan usando llaves {}.
+Propiedades: Es una asociacion entre un nombre (o clave) y un valor. La clave es el identificador y el valor es el valor que le queremos guardar. Se separan con coma. Los valores pueden ser cualquier tipo de dato que ya aprendimos. Ej.: RTCIceCandidatePairChangedEvent, numero, boolenano.. 
+Metodo: En los objetos, los valores se pueden establecer en funciones. Las funciones guardadas en un objeto se denominan agregarMetodoCalculoDescuento. Ej.: .lenght, .push, .pop. Lleva parentesis. 
+Bucle for  in: Los objetos no contienen indices numericos, por lo que el bucle estandar no funcionara. Javascript tiene un segundo tipo de bucle para los objetos, tiene un segundo tipo de bucle for integrado llamado For ... in loop. Podemos usar esta clave y los corchetes en nuestro bucle for para acceder al valor asociado a esa clave. 
+This : Hace referencia al objeto de donde estoy invocando el metodo (funcion). Es una palabra clave autorreferencial. 
