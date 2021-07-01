539e18f1e4dec06075a8f8d2cf377bb3c1c31f8a
unknown <you@example.com>
2021-07-01 17:24:43 -0300

    finishconcepts06-js-v

---
+++ 06-JS-V/concepts06-js-v.js
@@ -0,0 +1,7 @@
+Prototype: Javascript es un lenguaje que esta diseñado , esta basado en prototipos. Todos los objetos en Javascript tienen una referencia a otro objeto que es llamado su prototipo. Cuando creamos un objeto y busco una propiedad que no esta en el objeto, Javascript busca esa propiedad en el prototipo, si la propiedad no esta arroja error. 
+Los prototypos son un mecanismo mediante el cual los objetos en Js heredan caracteristicas ente si.
+Constructors(de Clases):  La funcion constructor nos va a permitir construir objetos(una fabrica de objetos). Todas las funciones constructoras de clases deben comenzar con mayuscula. 
+El metodo constructor es un metodo especial para crear e inicializar un objeto creado a partir de una clase. 
+Si no se especifica un metodo constructor, se utiliza un constructor predeterminado. Para las clases base, el constructor por defecto es: 
+    constructor () {}
+      
